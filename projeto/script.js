9d33604f30992b51a6f601303e40ebf193779871
Israel Amarante <109821140+israelamarante@users.noreply.github.com>
2023-09-22 10:23:47 -0300

    :tada: commit

---
+++ projeto/script.js
@@ -0,0 +1,11 @@
+function togglemode() {
+    const html = document.documentElement
+
+    /*if (html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('Light')
+    }*/
+
+    html.classList.toggle('light')    
+}
